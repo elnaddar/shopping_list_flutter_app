26a322728b07f4a517c6227e7de709c1076d9567
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-08 11:07:33 +0300

    finished form submission.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shopping_list_flutter_app/data/categories.dart';
+import 'package:shopping_list_flutter_app/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,6 +11,21 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> _formNamesAndValues = {
+    'category': categories[Categories.vegetables]
+  };
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
+  }
+
+  void _onSaved(String name, dynamic value) {
+    _formNamesAndValues[name] = value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +35,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -28,6 +43,7 @@ class _NewItemState extends State<NewItem> {
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   label: Text("Name"),
+                  errorMaxLines: 3,
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty || value.trim().isEmpty) {
@@ -37,8 +53,11 @@ class _NewItemState extends State<NewItem> {
                   }
                   return null;
                 },
+                onSaved: (newValue) => _onSaved("name", newValue),
               ),
+              const SizedBox(height: 12),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Expanded(
                     flex: 2,
@@ -46,6 +65,7 @@ class _NewItemState extends State<NewItem> {
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         label: Text("Quantity"),
+                        errorMaxLines: 3,
                       ),
                       initialValue: '1',
                       keyboardType: TextInputType.number,
@@ -55,12 +75,14 @@ class _NewItemState extends State<NewItem> {
                             value.isEmpty ||
                             value.trim().isEmpty ||
                             int.tryParse(value) == null) {
-                          return "This field is required and cannot be empty, be only white spaces, or have any character.";
+                          return "This field is required and cannot be empty.";
                         } else if (int.tryParse(value)! <= 0) {
                           return "Quantity must be 1 or more.";
                         }
                         return null;
                       },
+                      onSaved: (newValue) =>
+                          _onSaved("quantity", int.parse(newValue!)),
                     ),
                   ),
                   const SizedBox(
@@ -69,6 +91,7 @@ class _NewItemState extends State<NewItem> {
                   Expanded(
                     flex: 3,
                     child: DropdownButtonFormField(
+                      value: _formNamesAndValues["category"],
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         label: Text("Category"),
@@ -90,6 +113,7 @@ class _NewItemState extends State<NewItem> {
                           )
                       ],
                       onChanged: (value) {},
+                      onSaved: (newValue) => _onSaved("category", newValue),
                     ),
                   )
                 ],
@@ -99,12 +123,12 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () => _formKey.currentState!.reset(),
                     child: const Text("Reset"),
                   ),
                   const SizedBox(width: 6),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: _saveItem,
                     child: const Text("Add Item"),
                   ),
                 ],
