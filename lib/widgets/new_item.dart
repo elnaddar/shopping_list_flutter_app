f5c3795be3b02279a29889bb14f092e202f99361
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-08 10:14:23 +0300

    made ad new item form UI

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shopping_list_flutter_app/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -16,7 +19,77 @@ class _NewItemState extends State<NewItem> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: const Text("form"),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Name"),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Quantity"),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Category"),
+                      ),
+                      items: [
+                        for (final cat in categories.entries)
+                          DropdownMenuItem(
+                            value: cat.value,
+                            child: Row(
+                              children: [
+                                SizedBox.square(
+                                  dimension: 16,
+                                  child: ColoredBox(color: cat.value.color),
+                                ),
+                                const SizedBox(width: 6),
+                                Text(cat.value.name)
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Reset"),
+                  ),
+                  const SizedBox(width: 6),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Add Item"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
